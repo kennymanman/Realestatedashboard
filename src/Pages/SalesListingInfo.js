d99bc52de69b5e7832b7e0110f0ddf0624a9975b
kennymanman <61712644+kennymanman@users.noreply.github.com>
2023-11-30 23:59:24 -1200

    Latest commit

--- src/Pages/SalesListingInfo.js
+++ src/Pages/SalesListingInfo.js
@@ -521,7 +521,7 @@ type="file"
 
 
 
-<div className="h-screen flex">
+{/* <div className="h-screen flex">
                 <Pannellum
                     width="100%"
                     height="100%"
@@ -537,17 +537,10 @@ type="file"
                         console.log("panorama loaded");
                     }}
                 >
-                    {/* <Pannellum.Hotspot
-                        type="custom"
-                        pitch={-10}
-                        yaw={-120}
-                        handleClick={(evt, name) =>
-                            setCurrentScene(currentScene)
-                        }
-                    /> */}
+                  
                 </Pannellum>
 
-            </div>
+            </div> */}
 
 
 
